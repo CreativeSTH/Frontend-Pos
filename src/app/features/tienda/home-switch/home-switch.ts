8764a59bd6dabc775a71b51f10cc2a9f96f81154
CreativeSTH <sth.frontend@gmail.com>
2026-08-26 00:29:56 -0500

    feat: plantilla Foundry

--- src/app/features/tienda/home-switch/home-switch.ts
+++ src/app/features/tienda/home-switch/home-switch.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TiendaContextService } from '../../../core/services/tienda-context.service';
 import { AuroraHome } from '../plantillas/aurora/aurora-home';
 import { AtelierHome } from '../plantillas/atelier/atelier-home';
+import { FoundryHome } from '../plantillas/foundry/foundry-home';
 
 @Component({
   selector: 'app-tienda-home-switch',
   standalone: true,
-  imports: [AuroraHome, AtelierHome],
+  imports: [AuroraHome, AtelierHome, FoundryHome],
   templateUrl: './home-switch.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
