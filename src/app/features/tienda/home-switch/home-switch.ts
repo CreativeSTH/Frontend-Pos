feefe399e6362aaf30c05c8e9cac9a2136c70014
CreativeSTH <sth.frontend@gmail.com>
2026-08-26 00:28:23 -0500

    feat: plantilla Atelier

--- src/app/features/tienda/home-switch/home-switch.ts
+++ src/app/features/tienda/home-switch/home-switch.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TiendaContextService } from '../../../core/services/tienda-context.service';
 import { AuroraHome } from '../plantillas/aurora/aurora-home';
+import { AtelierHome } from '../plantillas/atelier/atelier-home';
 
 @Component({
   selector: 'app-tienda-home-switch',
   standalone: true,
-  imports: [AuroraHome],
+  imports: [AuroraHome, AtelierHome],
   templateUrl: './home-switch.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
